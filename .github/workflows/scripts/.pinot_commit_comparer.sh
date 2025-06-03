7219b2a132c72e1c793b4b7e78d54a679019e5ae
Matthew Jack <mjack@linkedin.com>
2025-06-03 14:57:17 -0700

    switching to a different set of commits to avoid the errors with pinot-controller. also commented out japicmp code to focus on potential issues with the branch checking-out

--- .github/workflows/scripts/.pinot_commit_comparer.sh
+++ .github/workflows/scripts/.pinot_commit_comparer.sh
@@ -15,45 +15,49 @@ version="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr 
 modnames="$(mvn -pl :pinot help:effective-pom | grep "<module>" | tr -d "/<>" | tr "\n" " ")"
 modnames=${modnames//"module"} # removes the word 'module' from the output
 IFS=' ' read -r -a namelist <<< "$modnames"
-#echo "${namelist[0]}"
-#echo "${namelist[1]}"
-#echo "${namelist[2]}"
-#echo "${namelist[3]}"
-git checkout 608f891
+#for item in "${namelist[@]}"; do
+#  echo "$item"
+#done
+
+# use github cli to get pr-level operations(?)
+# pr-level and commit level is different
+
+# originally did 608f891 vs c7ce654 for common and controller (4 8)
+# but there was some error with compiling controller that showed up in master too
+# so pivoting to my broker-server commit
+git checkout b061c55
 #mvn clean package -pl $ -DskipTests
-temp=(4 8) #hardcoding modules that changed between these specific commits
-for i in {0..1}; do
-  j="${temp[i]}"
-  mvn clean package -pl "${namelist[j]}" -DskipTests
-  mv "${namelist[j]}"/target/"${namelist[j]}"-"$version".jar commit_jars_frst
+temp=(1 3 6) #hardcoding modules that changed between these specific commits
+for num in "${temp[@]}"; do
+  mvn clean install -pl "${namelist[num]}" -DskipTests
+  mv "${namelist[num]}"/target/"${namelist[num]}"-"$version".jar commit_jars_frst
 done
-git checkout c7ce654
-for i in {0..1}; do
-  j="${temp[i]}"
-  mvn clean package -pl "${namelist[j]}" -DskipTests
-  mv "${namelist[j]}"/target/"${namelist[j]}"-"$version".jar commit_jars_scnd
+git checkout f0c9638
+for num in "${temp[@]}"; do
+  mvn clean install -pl "${namelist[num]}" -DskipTests
+  mv "${namelist[num]}"/target/"${namelist[num]}"-"$version".jar commit_jars_scnd
 done
 git checkout commit-report/japicmp_test
-
-JAPICMP_VER=0.23.1
-curl -sLo japicmp.jar "https://repo1.maven.org/maven2/org/japicmp/japicmp/$JAPICMP_VER/japicmp-$JAPICMP_VER-jar-with-dependencies.jar"
-
-# Ensure the download was successful (optional but recommended)
-if [ ! -f japicmp.jar ]; then
-  echo "Error: Failed to download japicmp.jar."
-  exit 1
-fi
-
-for i in {0..1}; do
-  j="${temp[i]}"
-  OLD=commit_jars_frst/"${namelist[j]}"-"$version".jar
-  NEW=commit_jars_scnd/"${namelist[j]}"-"$version".jar
-  java -jar japicmp.jar \
-    --old "$OLD" \
-    --new "$NEW" \
-    --error-on-source-incompatibility \
-    --only-incompatible
-done
+#
+#JAPICMP_VER=0.23.1
+#curl -sLo japicmp.jar "https://repo1.maven.org/maven2/org/japicmp/japicmp/$JAPICMP_VER/japicmp-$JAPICMP_VER-jar-with-dependencies.jar"
+#
+## Ensure the download was successful (optional but recommended)
+#if [ ! -f japicmp.jar ]; then
+#  echo "Error: Failed to download japicmp.jar."
+#  exit 1
+#fi
+#
+#for i in {0..1}; do
+#  j="${temp[i]}"
+#  OLD=commit_jars_frst/"${namelist[j]}"-"$version".jar
+#  NEW=commit_jars_scnd/"${namelist[j]}"-"$version".jar
+#  java -jar japicmp.jar \
+#    --old "$OLD" \
+#    --new "$NEW" \
+#    --error-on-source-incompatibility \
+#    --only-incompatible
+#done
 
 #javac -d pinot-commit-reporter/target/classes pinot-commit-reporter/src/main/java/org/apache/pinot/committer/JarIterator.java
 #java -cp pinot-commit-reporter/target/classes org.apache.pinot.committer.JarIterator "$modnames"
