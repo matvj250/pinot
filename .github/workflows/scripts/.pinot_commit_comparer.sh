1dbaa6383e7167dae42200f106ae4cc2f9b25f23
Matthew Jack <mjack@linkedin.com>
2025-06-03 11:34:48 -0700

    minor fix

--- .github/workflows/scripts/.pinot_commit_comparer.sh
+++ .github/workflows/scripts/.pinot_commit_comparer.sh
@@ -4,17 +4,17 @@ if [ -d commit_jars_frst ]; then
   rm -r commit_jars_frst
 fi
 if [ -d commit_jars_scnd ]; then
-  rm -r commit_jars_scnd/*
+  rm -r commit_jars_scnd
 fi
 mkdir commit_jars_frst
 version="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr -d "%")" #there's a % at the end for some reason
 modnames="$(mvn -pl :pinot help:effective-pom | grep "<module>" | tr -d "/<>" | tr "\n" " ")"
 modnames=${modnames//"module"} # removes the word 'module' from the output
 IFS=' ' read -r -a namelist <<< "$modnames"
-echo "${namelist[0]}"
-echo "${namelist[1]}"
-echo "${namelist[2]}"
-echo "${namelist[3]}"
+#echo "${namelist[0]}"
+#echo "${namelist[1]}"
+#echo "${namelist[2]}"
+#echo "${namelist[3]}"
 git checkout 608f891
 #mvn clean package -pl $ -DskipTests
 for i in {0..1}; do
