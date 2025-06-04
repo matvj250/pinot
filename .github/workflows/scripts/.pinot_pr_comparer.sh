75ee8bee06bc9e72658f8b059710de382d8ba3f8
Matthew Jack <mjack@linkedin.com>
2025-06-04 15:33:34 -0700

    changed it so that name list generation happened after the first checkout

--- .github/workflows/scripts/.pinot_pr_comparer.sh
+++ .github/workflows/scripts/.pinot_pr_comparer.sh
@@ -12,17 +12,18 @@ fi
 mkdir commit_jars_old
 mkdir commit_jars_new
 
-version="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr -d "%")" #there's a % at the end for some reason
-modnames="$(mvn -pl :pinot help:effective-pom | grep "<module>" | tr -d "/<>" | tr "\n" " ")"
-modnames=${modnames//"module"} # removes the word 'module' from the output
-IFS=' ' read -r -a namelist <<< "$modnames"
-
 #second and third latest for now, because at the time of making this
 #the latest change was dependabot
 gh repo set-default apache/pinot
 latest=15685
 #"$(gh pr list --state merged --json number,mergedAt --limit 50 | jq 'sort_by(.mergedAt) | reverse | .[0].number')"
 gh pr checkout "$latest"
+
+version="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr -d "%")" #there's a % at the end for some reason
+modnames="$(mvn -pl :pinot help:effective-pom | grep "<module>" | tr -d "/<>" | tr "\n" " ")"
+modnames=${modnames//"module"} # removes the word 'module' from the output
+IFS=' ' read -r -a namelist <<< "$modnames"
+
 for name in "${namelist[@]}"; do # eventually remove temp and switch to namelist directly
   mvn clean install -pl "$name" -DskipTests
   mv "$name"/target/"$name"-"$version".jar commit_jars_new
@@ -59,7 +60,7 @@ else
 fi
 for name in "${namelist[@]}"; do
   if [ ! -e commit_jars_old/"$name"-"$version".jar ] || [ ! -e commit_jars_new/"$name"-"$version".jar ]; then
-    echo "It seems ${namelist[num]} does not exist in one or both of the pull requests. Please look into this." >> japicmp.txt
+    echo "It seems ${namelist[num]} does not exist in one or both of the pull requests. Please look into this." >> japicmp_test.txt
     continue
   fi
   OLD=commit_jars_old/"${name}"-"$version".jar
@@ -68,6 +69,5 @@ for name in "${namelist[@]}"; do
     --old "$OLD" \
     --new "$NEW" \
     --no-annotations \
-    --ignore-missing-classes \
-    --only-modified >> japicmp_test.txt
+    --ignore-missing-classes >> japicmp_test.txt
 done
