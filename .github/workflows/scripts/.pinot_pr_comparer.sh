d7aa9622f0424aa9444f72c95256edb385bc3348
Matthew Jack <mjack@linkedin.com>
2025-06-05 11:15:31 -0700

    commenting out code so i can use jars that already exist for quicker testing

--- .github/workflows/scripts/.pinot_pr_comparer.sh
+++ .github/workflows/scripts/.pinot_pr_comparer.sh
@@ -4,26 +4,26 @@
 if [ -d commit_jars_old ]; then
   rm -r commit_jars_old
 fi
-if [ -d commit_jars_new ]; then
-  rm -r commit_jars_new
-fi
+#if [ -d commit_jars_new ]; then
+#  rm -r commit_jars_new
+#fi
 mkdir commit_jars_old
-mkdir commit_jars_new
+#mkdir commit_jars_new
 
 gh repo set-default apache/pinot
 prnums="$(gh pr list --state merged --json number,mergedAt | jq 'sort_by(.mergedAt) | reverse')"
 latest=$(echo "$prnums" | jq '.[0].number')
-gh pr checkout "$latest"
+#gh pr checkout "$latest"
 
 version="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr -d "%")" # there's a % at the end for some reason
-mvn clean install -DskipTests
-paths="$(find . -type f -name "*${version}.jar" | tr "\n" " ")"
-IFS=' ' read -r -a namelist <<< "$paths"
-for name in "${namelist[@]}"; do
-  if [ -f "$name" ]; then
-    mv "$name" commit_jars_new
-  fi
-done
+#mvn clean install -DskipTests
+#paths="$(find . -type f -name "*${version}.jar" | tr "\n" " ")"
+#IFS=' ' read -r -a namelist <<< "$paths"
+#for name in "${namelist[@]}"; do
+#  if [ -f "$name" ]; then
+#    mv "$name" commit_jars_new
+#  fi
+#done
 
 sndlatest=$(echo "$prnums" | jq '.[1].number')
 gh pr checkout "$sndlatest"
