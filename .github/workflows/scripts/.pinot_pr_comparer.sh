eeb11abeb4020475f49be567e2eb45aeab7df4e7
Matthew Jack <mjack@linkedin.com>
2025-06-05 10:56:26 -0700

    uncommenting code because testing worked

--- .github/workflows/scripts/.pinot_pr_comparer.sh
+++ .github/workflows/scripts/.pinot_pr_comparer.sh
@@ -16,10 +16,7 @@ latest=$(echo "$prnums" | jq '.[0].number')
 gh pr checkout "$latest"
 
 version="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr -d "%")" # there's a % at the end for some reason
-temp=("pinot-dependency-verifier" "pinot-spi") #hardcoding modules that changed between these specific commits
-for num in "${temp[@]}"; do
-  mvn clean install -DskipTests -pl "$num"
-done
+mvn clean install -DskipTests
 paths="$(find . -type f -name "*${version}.jar" | tr "\n" " ")"
 IFS=' ' read -r -a namelist <<< "$paths"
 for name in "${namelist[@]}"; do
@@ -28,50 +25,50 @@ for name in "${namelist[@]}"; do
   fi
 done
 
-#sndlatest=$(echo "$prnums" | jq '.[1].number')
-#gh pr checkout "$sndlatest"
-#mvn clean install -DskipTests
-#paths2="$(find . -type f -name "*${version}.jar" | tr "\n" " ")"
-#IFS=' ' read -r -a namelist2 <<< "$paths2"
-#for name in "${namelist2[@]}"; do
-#  if [ -f "$name"/target/"$name"-"$version".jar ]; then
-#    mv "$name"/target/"$name"-"$version".jar commit_jars_old
-#    fi
-#done
-#
+sndlatest=$(echo "$prnums" | jq '.[1].number')
+gh pr checkout "$sndlatest"
+mvn clean install -DskipTests
+paths2="$(find . -type f -name "*${version}.jar" | tr "\n" " ")"
+IFS=' ' read -r -a namelist2 <<< "$paths2"
+for name in "${namelist2[@]}"; do
+  if [ -f "$name"/target/"$name"-"$version".jar ]; then
+    mv "$name"/target/"$name"-"$version".jar commit_jars_old
+    fi
+done
+
 #TODO: change the below to just checking out the gh-pages branch
 gh repo set-default matvj250/pinot
 git checkout commit-report/japicmp_test
-#
-#if [ ! -f japicmp.jar ]; then
-#  JAPICMP_VER=0.23.1
-#  curl -fSL \
-#  -o japicmp.jar \
-#  "https://repo1.maven.org/maven2/com/github/siom79/japicmp/japicmp/${JAPICMP_VER}/japicmp-${JAPICMP_VER}-jar-with-dependencies.jar"
-#  if [ ! -f japicmp.jar ]; then
-#    echo "Error: Failed to download japicmp.jar."
-#    exit 1
-#  fi
-#fi
-#
-#if [ -e japicmp_test.txt ]; then
-#  echo "" > japicmp_test.txt # erase what's in the text already
-#else
-#  touch japicmp_test.txt
-#fi
-#for filename in commit_jars_new/*; do
-#  name="$(basename "$filename")"
-#  if [ ! -f commit_jars_old/"name" ]; then
-#    echo "It seems $name does not exist in the previous pull request. Please make sure this is intended." >> japicmp_test.txt
-#    echo "" >> japicmp_test.txt
-#    continue
-#  fi
-#  OLD=commit_jars_old/"$name"
-#  NEW=commit_jars_new/"$name"
-#  java -jar japicmp.jar \
-#    --old "$OLD" \
-#    --new "$NEW" \
-#    --no-annotations \
-#    --ignore-missing-classes \
-#    --only-modified >> japicmp_test.txt
-#done
+
+if [ ! -f japicmp.jar ]; then
+  JAPICMP_VER=0.23.1
+  curl -fSL \
+  -o japicmp.jar \
+  "https://repo1.maven.org/maven2/com/github/siom79/japicmp/japicmp/${JAPICMP_VER}/japicmp-${JAPICMP_VER}-jar-with-dependencies.jar"
+  if [ ! -f japicmp.jar ]; then
+    echo "Error: Failed to download japicmp.jar."
+    exit 1
+  fi
+fi
+
+if [ -e japicmp_test.txt ]; then
+  echo "" > japicmp_test.txt # erase what's in the text already
+else
+  touch japicmp_test.txt
+fi
+for filename in commit_jars_new/*; do
+  name="$(basename "$filename")"
+  if [ ! -f commit_jars_old/"name" ]; then
+    echo "It seems $name does not exist in the previous pull request. Please make sure this is intended." >> japicmp_test.txt
+    echo "" >> japicmp_test.txt
+    continue
+  fi
+  OLD=commit_jars_old/"$name"
+  NEW=commit_jars_new/"$name"
+  java -jar japicmp.jar \
+    --old "$OLD" \
+    --new "$NEW" \
+    --no-annotations \
+    --ignore-missing-classes \
+    --only-modified >> japicmp_test.txt
+done
