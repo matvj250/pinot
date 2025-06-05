fdd4698a083cfc58bce41b73af2bb6d0d7d21916
Matthew Jack <mjack@linkedin.com>
2025-06-05 14:05:45 -0700

    fixed issue with pruning and finding files

--- .github/workflows/scripts/.bash_code_test.sh
+++ .github/workflows/scripts/.bash_code_test.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
-temp="$(find . -type f -name "*1.4.0-SNAPSHOT.jar" | tr "\n" " ")"
-temp=${temp//".//"}
-IFS=' ' read -r -a namelist <<< "$temp"
-echo "${namelist[@]}"
+if [ ! -f japicmp.jar ]; then
+  JAPICMP_VER=0.23.1
+  curl -fSL \
+  -o japicmp.jar \
+  "https://repo1.maven.org/maven2/com/github/siom79/japicmp/japicmp/${JAPICMP_VER}/japicmp-${JAPICMP_VER}-jar-with-dependencies.jar"
+  if [ ! -f japicmp.jar ]; then
+    echo "Error: Failed to download japicmp.jar."
+    exit 1
+  fi
+fi
 
+if [ -e japicmp_test.txt ]; then
+  echo "" > japicmp_test.txt # erase what's in the text already
+else
+  touch japicmp_test.txt
+fi
+for filename in commit_jars_new/*; do
+  name="$(basename "$filename")"
+  if [ ! -f commit_jars_old/"$name" ]; then
+    echo "It seems $name does not exist in the previous pull request. Please make sure this is intended." >> japicmp_test.txt
+    echo "" >> japicmp_test.txt
+    continue
+  fi
+  OLD=commit_jars_old/"$name"
+  NEW=commit_jars_new/"$name"
+  java -jar japicmp.jar \
+    --old "$OLD" \
+    --new "$NEW" \
+    --no-annotations \
+    --ignore-missing-classes \
+    --only-modified >> japicmp_test.txt
+done
 #modnames="$(mvn -pl :pinot help:effective-pom -amd | grep "<module>" | tr -d "/<>" | tr "\n" " ")"
 #modnames=${modnames//"module"} # removes the word 'module' from the output
 #IFS=' ' read -r -a namelist <<< "$modnames"
